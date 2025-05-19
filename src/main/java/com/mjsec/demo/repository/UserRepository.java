7f509caf4fdaf132d90f5af62f89292d4490af79
MEspeaker <sho3492@gmail.com>
2025-05-19 23:23:22 +0900

    feat(join/login): add join/login

--- src/main/java/com/mjsec/demo/repository/UserRepository.java
+++ src/main/java/com/mjsec/demo/repository/UserRepository.java
@@ -4,5 +4,7 @@ import com.mjsec.demo.domain.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+    Boolean existsByUsername(String username);
 
+    UserEntity findByUsername(String username);
 }
